87c5da6a15cbd51307796ae79d565df4d1ed81b3
jj-2206 <jinju.woo2022@gmail.com>
2022-08-18 17:32:20 +0900

    fix: Add onToggle checkbox checked

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -2,18 +2,16 @@ import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react
 import './TodoListItem.scss';
 import cn from 'classnames';
 
-const TodoListItem = ({ todo, onRemove }) => {
+const TodoListItem = ({ todo, onRemove, onToggle }) => {
   const { id, text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })}>
-        <div className="checkbox">
-          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-          <div className="text">{text}</div>
-        </div>
-        <div className="remove" onClick={() => onRemove(id)}>
-          <MdRemoveCircleOutline />
-        </div>
+      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove" onClick={() => onRemove(id)}>
+        <MdRemoveCircleOutline />
       </div>
     </div>
   );
